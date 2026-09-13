95d6d7eda99d5ffa544763109d54437b9fd282db
Poornima <poornima13206@gmail.com>
2026-09-13 22:24:38 +0530

    Create While_loop.java
    add loops concept

---
+++ 8 Loops/While_loop.java
@@ -0,0 +1,31 @@
+//package 8 Loops;
+ import java.util.Scanner;
+public class While_loop {
+ 
+  public static void main(String[] args) {
+    // int i = 1;    // Initialize i to 1
+    // while (i <= 10) {   //condition
+    //   System.out.println(i); // Print the value of i
+    //   i= i + 1; // i++ also works //updating the condition
+    // }
+
+    int i = 10;
+    while(i>=1){                 //number reverse order
+      System.out.println(i);
+      i--;
+    }
+
+    // int i = 1;
+    // while(i<=10){
+    //   Scanner scanner = new Scanner(System.in);
+    //   System.out.println("Enter a number: ");
+    //   int number = scanner.nextInt();
+    //   System.out.println("You entered: " + number);
+    //   i++;
+    
+    // }
+   // scanner.close();
+
+  }
+
+}
