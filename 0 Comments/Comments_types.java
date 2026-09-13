d3c1aef33eec9d55258105f541c6d31fc0bed716
Poornima <poornima13206@gmail.com>
2026-09-13 22:24:10 +0530

    Create Comments_types.java
    add comment concept

---
+++ 0 Comments/Comments_types.java
@@ -0,0 +1,19 @@
+
+public class Comments_types {
+  
+public static void main(String[] args) {
+    // This is a single-line comment
+
+    /*
+     * This is a multi-line comment
+     * It can span multiple lines
+     */
+
+    /**                                    put /** then enter to create a documentation comment
+     * This is a documentation comment
+     * It is used to generate API documentation
+     */
+
+    System.out.println("Comments in Java");
+  }
+}
